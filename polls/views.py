c9e47fcec6ca7208defb870819bf3c320a2a1090
DineshNayak <dinesh@cognitiveclouds.com>
2014-10-21 18:07:54 +0530

    some more views

--- polls/views.py
+++ polls/views.py
@@ -7,3 +7,15 @@ def index(request):
     return HttpResponse("Hello world. You are at the polls index. ")
 
 
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You were looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You were voting on question %s." % question_id)
+
